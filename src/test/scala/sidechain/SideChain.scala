0301bf3a8507c5bbfc5186792ecad660594c70dc
ross-weir <29697678+ross-weir@users.noreply.github.com>
2023-10-13 20:53:16 +1100

    add basic test case skeletons

---
+++ src/test/scala/sidechain/SideChain.scala
@@ -0,0 +1,9 @@
+package sidechain
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should
+
+
+class SideChain extends AnyFlatSpec with should.Matchers {
+  "unlock" should "mint sideERG if valid proof of locking on main chain is provided" {}
+}
