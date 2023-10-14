4a5b08cce6603475055e26503e3af7d01da2be76
Alexander Chepurnoy <alex.chepurnoy@iohk.io>
2023-10-14 20:23:24 +0300

    WP - title & abstract

---
+++ docs/whitepaper/compile.sh
@@ -0,0 +1,14 @@
+rm sidechain.aux
+rm sidechain.out
+rm sidechain.log
+rm sidechain.bbl
+rm sidechain.blg
+pdflatex sidechain
+bibtex sidechain
+pdflatex sidechain
+pdflatex sidechain
+rm sidechain.aux
+rm sidechain.out
+rm sidechain.log
+rm sidechain.bbl
+rm sidechain.blg
